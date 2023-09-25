aad336ee046e8745969f030f932568a3d975763c
Tormenta88 <tormenta8282@gmail.com>
2023-09-25 17:31:02 +0200

    Pruebas

--- dajdjwad.js
+++ dajdjwad.js
@@ -1,11 +1,14 @@
 function beforerender(cmp){};
 function afterrender(cmp){};
 
+const rotaciones = ["rotate(90deg)","rotate(270deg)","rotate(180deg)"];
+
+
 function rotar(){
     for (const [key, value] of Object.entries(correctas)) {
         console.log(key, value);
         if (value == false){
-            document.getElementById(key).style.transform = "rotate(90deg)"
+            document.getElementById(key).style.transform = rotaciones[(Math.floor(Math.random() * rotaciones.length))];
         };
     };
 }
